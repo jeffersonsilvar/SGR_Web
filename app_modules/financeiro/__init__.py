5919aa91133e4047f7e513d6ff36416a6e5c5a5b
Jefferson Silva <jeffersonsilvar.lima@gmail.com>
2026-08-19 13:02:18 -0300

    feat: registra rota de edicao de titulos manuais

--- app_modules/financeiro/__init__.py
+++ app_modules/financeiro/__init__.py
@@ -1,3 +1,11 @@
-from .routes import criar_financeiro_blueprint
+from .edicao_titulo import registrar_rotas_edicao_titulo
+from .routes import criar_financeiro_blueprint as _criar_financeiro_blueprint_base
+
+
+def criar_financeiro_blueprint(services):
+    financeiro_bp = _criar_financeiro_blueprint_base(services)
+    registrar_rotas_edicao_titulo(financeiro_bp, services)
+    return financeiro_bp
+
 
 __all__ = ["criar_financeiro_blueprint"]
