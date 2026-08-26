559d237a12292b21509ad92a08d9bfe9bce0ab93
Jefferson Silva <jeffersonsilvar.lima@gmail.com>
2026-08-26 16:55:25 -0300

    feat: registra importacao inteligente de xml

--- app_modules/documentos/__init__.py
+++ app_modules/documentos/__init__.py
@@ -1,10 +1,12 @@
 from .routes import criar_documentos_blueprint as _criar_documentos_blueprint_base
 from .workflow import registrar_fluxo_documental
+from .importacao_xml import registrar_importacao_xml
 
 
 def criar_documentos_blueprint(services):
     documentos_bp = _criar_documentos_blueprint_base(services)
     registrar_fluxo_documental(documentos_bp, services)
+    registrar_importacao_xml(documentos_bp, services)
     return documentos_bp
 
 
