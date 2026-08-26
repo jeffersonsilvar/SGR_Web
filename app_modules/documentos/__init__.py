54c885150e7619b6e16dba74ad0b5013bbed78eb
Jefferson Silva <jeffersonsilvar.lima@gmail.com>
2026-08-26 16:11:35 -0300

    refactor: registra workflow de documentos fiscais

--- app_modules/documentos/__init__.py
+++ app_modules/documentos/__init__.py
@@ -1,3 +1,11 @@
-from .routes import criar_documentos_blueprint
+from .routes import criar_documentos_blueprint as _criar_documentos_blueprint_base
+from .workflow import registrar_fluxo_documental
+
+
+def criar_documentos_blueprint(services):
+    documentos_bp = _criar_documentos_blueprint_base(services)
+    registrar_fluxo_documental(documentos_bp, services)
+    return documentos_bp
+
 
 __all__ = ["criar_documentos_blueprint"]
