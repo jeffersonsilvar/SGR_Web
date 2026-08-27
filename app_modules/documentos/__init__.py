73a6899509abf0090bd439fcf548a120449877f6
Jefferson Silva <jeffersonsilvar.lima@gmail.com>
2026-08-27 12:31:57 -0300

    Ativa cadastro manual fiscal via StorageService

--- app_modules/documentos/__init__.py
+++ app_modules/documentos/__init__.py
@@ -3,6 +3,7 @@ from .workflow import registrar_fluxo_documental
 from . import importacao_xml as _importacao_xml
 from .visualizacao_fiscal import registrar_visualizacao_fiscal
 from .cte_importacao import instalar_suporte_cte
+from .cadastro_manual_storage import registrar_cadastro_manual_storage
 
 
 instalar_suporte_cte(_importacao_xml)
@@ -11,6 +12,7 @@ registrar_importacao_xml = _importacao_xml.registrar_importacao_xml
 
 def criar_documentos_blueprint(services):
     documentos_bp = _criar_documentos_blueprint_base(services)
+    registrar_cadastro_manual_storage(documentos_bp, services)
     registrar_fluxo_documental(documentos_bp, services)
     registrar_importacao_xml(documentos_bp, services)
     registrar_visualizacao_fiscal(documentos_bp, services)
