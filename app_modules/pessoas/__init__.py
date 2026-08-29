fea33fecb884d6bb7383a1fc83a2a849265b3de9
Jefferson Silva <jeffersonsilvar.lima@gmail.com>
2026-08-29 13:15:03 -0300

    feat: iniciar domínio de pessoas e vínculos

---
+++ app_modules/pessoas/__init__.py
@@ -0,0 +1,13 @@
+from .vinculos import (
+    TIPOS_VINCULO,
+    listar_vinculos_pessoa,
+    normalizar_tipo_vinculo,
+    pessoa_possui_vinculo,
+)
+
+__all__ = [
+    "TIPOS_VINCULO",
+    "listar_vinculos_pessoa",
+    "normalizar_tipo_vinculo",
+    "pessoa_possui_vinculo",
+]
