845dc49c5189da57412308a67f0ba636aa14b3e7
Jefferson Silva <jeffersonsilvar.lima@gmail.com>
2026-08-29 16:41:14 -0300

    refactor: exportar sincronizacao de vinculos de pessoas

--- app_modules/pessoas/__init__.py
+++ app_modules/pessoas/__init__.py
@@ -1,13 +1,21 @@
 from .vinculos import (
     TIPOS_VINCULO,
+    VINCULOS_GERENCIADOS_CADASTRO,
+    condicao_sql_vinculo_pessoa,
     listar_vinculos_pessoa,
     normalizar_tipo_vinculo,
     pessoa_possui_vinculo,
+    sincronizar_vinculos_por_cadastro,
+    tipos_vinculo_derivados,
 )
 
 __all__ = [
     "TIPOS_VINCULO",
+    "VINCULOS_GERENCIADOS_CADASTRO",
+    "condicao_sql_vinculo_pessoa",
     "listar_vinculos_pessoa",
     "normalizar_tipo_vinculo",
     "pessoa_possui_vinculo",
+    "sincronizar_vinculos_por_cadastro",
+    "tipos_vinculo_derivados",
 ]
